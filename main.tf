38b7c89775f98e8d3146e1a44a827f4ecede749c
Jiho Lee <optional.int@kakao.com>
2023-08-11 23:30:12 +0900

    feat: provision clb server using terraform / ansible

--- main.tf
+++ main.tf
@@ -11,22 +11,22 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-resource "aws_lightsail_key_pair" "ovpn_server" {
-  name       = "ovpn_server_key_pair"
-  public_key = file("~/.ssh/ovpn_server.pub")
+resource "aws_lightsail_key_pair" "clb_server" {
+  name       = "clb_server_key_pair"
+  public_key = file("~/.ssh/clb_server.pub")
 }
 
-resource "aws_lightsail_instance" "ovpn_server" {
-  depends_on = [aws_lightsail_key_pair.ovpn_server]
-  name              = "ovpn_server_instance"
+resource "aws_lightsail_instance" "clb_server" {
+  depends_on = [aws_lightsail_key_pair.clb_server]
+  name              = "clb_server_instance"
   availability_zone = "ap-northeast-1c"
   blueprint_id      = "ubuntu_22_04"
   bundle_id         = "nano_2_0"
-  key_pair_name     = "ovpn_server_key_pair"
+  key_pair_name     = "clb_server_key_pair"
 }
 
-resource "aws_lightsail_instance_public_ports" "ovpn_server" {
-  instance_name = aws_lightsail_instance.ovpn_server.name
+resource "aws_lightsail_instance_public_ports" "clb_server" {
+  instance_name = aws_lightsail_instance.clb_server.name
 
   port_info {
     protocol  = "tcp"
@@ -48,30 +48,30 @@ resource "aws_lightsail_instance_public_ports" "ovpn_server" {
 }
 
 resource "null_resource" "ansible_exec" {
-    depends_on = [aws_lightsail_instance.ovpn_server]
+    depends_on = [aws_lightsail_instance.clb_server]
     provisioner "local-exec" {
 		working_dir = "${path.module}"
         command = <<EOF
 			set -x
 
-			export ovpn_user=${aws_lightsail_instance.ovpn_server.username}
-			export ovpn_ip=${aws_lightsail_instance.ovpn_server.public_ip_address}
+			export clb_user=${aws_lightsail_instance.clb_server.username}
+			export clb_ip=${aws_lightsail_instance.clb_server.public_ip_address}
 
 			export ANSIBLE_PERSISTENT_CONNECT_TIMEOUT=9999
 			export ANSIBLE_PERSISTENT_COMMAND_TIMEOUT=9999
 			export ANSIBLE_HOST_KEY_CHECKING=False 
 
-			rm -f inventory client.ovpn
+			rm -f inventory client.clb
 
-            echo "ovpn_server ansible_port=22 ansible_host=$ovpn_ip ansible_user=$ovpn_user ansible_ssh_private_key_file=~/.ssh/ovpn_server" > inventory
+            echo "clb_server ansible_port=22 ansible_host=$clb_ip ansible_user=$clb_user ansible_ssh_private_key_file=~/.ssh/clb_server" > inventory
             
-			while ! ssh -o StrictHostKeyChecking=accept-new -i ~/.ssh/ovpn_server $ovpn_user@$ovpn_ip whoami;
+			while ! ssh -o StrictHostKeyChecking=accept-new -i ~/.ssh/clb_server $clb_user@$clb_ip whoami;
 			do
 				sleep 1s
 			done
 
-			unset ovpn_user
-			unset ovpn_ip
+			unset clb_user
+			unset clb_ip
 
             ansible-playbook -i inventory playbook.yaml -v
 
